cd0a89b14ff19ae48f53c199850ec3dfb9e52986
JANGTAEHEE <jangth0655@gmail.com>
2022-09-06 09:37:01 +0900

    updated__document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta property="og:title" content="taehee's Homepage" />
         <meta property="og:description" content="TaeHee의 Homepage" />
-        <meta property="og:image" content="/public/logo/logo.png" />
+        <meta property="og:image" content="../public/logo/logo.png" />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700;900&display=swap"
           rel="stylesheet"
