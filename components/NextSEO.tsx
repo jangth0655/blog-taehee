46a8391068d3a865f238fb37e1656f8119e293ea
taehee <jangth0655@gmail.com>
2022-12-07 12:07:26 +0900

    [fix] nextseo title 수정

--- components/NextSEO.tsx
+++ components/NextSEO.tsx
@@ -7,11 +7,10 @@ interface Props {
 }
 
 const NextSEO = ({ description, title, isDetail = false }: Props) => {
-  const normalTitle = `Taehee's Blog | ${title || ''}`;
   const detailTitle = title || '';
   return (
     <NextSeo
-      title={isDetail ? detailTitle : normalTitle}
+      title={isDetail ? detailTitle : 'Taehee의 개발 블로그 입니다 :)'}
       description='javascript, typescript, react와 관련해서 학습한 내용을 기록하며 꾸준히 성장하고 있습니다.'
       openGraph={{
         type: 'website',
