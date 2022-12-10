2fc6a3ebf762c6cf3318dc8694725d1a1dcce9b2
taehee <jangth0655@gmail.com>
2022-12-10 20:57:05 +0900

    [fix] 시스템 theme 설정에 따른 에러 해결-resolvedTheme 추가

--- components/home/ThemeSwitch.tsx
+++ components/home/ThemeSwitch.tsx
@@ -1,30 +1,32 @@
 import { HiSun, HiMoon } from 'react-icons/hi';
 import { motion } from 'framer-motion';
-import { ThemeChanger } from '../../hooks/useTheme';
-import cls from '../../module/cls';
+import { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
 
 const DARK_MODE = 'dark';
-const LIGHT_MODE = 'light';
 
 const ThemeSwitch = () => {
-  const { theme, toggleTheme } = ThemeChanger();
-  const darkTheme = theme === DARK_MODE;
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme, resolvedTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
+
+  const darkTheme = theme === DARK_MODE || resolvedTheme === 'dark';
   const handleTheme = () => {
-    if (!toggleTheme) return;
-    darkTheme ? toggleTheme(LIGHT_MODE) : toggleTheme(DARK_MODE);
+    setTheme(theme === 'dark' || resolvedTheme === 'dark' ? 'light' : 'dark');
   };
 
   return (
     <div className='flex sm:w-20 w-14 border-2 dark:border-white border-zinc-700 items-center rounded-xl px-1 py-[1px] mr-4 absolute left-40 dark:text-zinc-200 text-zinc-700 justify-between'>
       <div onClick={handleTheme}>
-        {darkTheme ? (
+        {mounted && darkTheme ? (
           <motion.div layoutId='dark'>
             <HiSun />
           </motion.div>
         ) : null}
       </div>
       <div onClick={handleTheme}>
-        {darkTheme ? null : (
+        {mounted && darkTheme ? null : (
           <motion.div layoutId='dark'>
             <HiMoon />
           </motion.div>
