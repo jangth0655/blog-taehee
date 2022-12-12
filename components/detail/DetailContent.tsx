557ac62fb3724cb24872425744a1a26760b5d14d
taehee <jangth0655@gmail.com>
2022-12-13 00:15:55 +0900

    [design] blog post max-width 변경과 img width, height 변경, react-reconciliation 이미지 추가 및 내용 추가

--- components/detail/DetailContent.tsx
+++ components/detail/DetailContent.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 const DetailContent = ({ post }: Props) => {
   return (
-    <div className='max-w-2xl m-auto dark:text-zinc-300 text-zinc-700'>
+    <div className='max-w-3xl m-auto dark:text-zinc-300 text-zinc-700'>
       <div className='post' dangerouslySetInnerHTML={{ __html: post }} />
     </div>
   );
