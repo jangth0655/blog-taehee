0e051e42807ca3be85a1468fed9bec92ce882fbe
taehee <jangth0655@gmail.com>
2022-12-14 14:04:06 +0900

    [design] ListContent hover, mark(circle) 디자인 업데이트

--- components/detail/ListContent.tsx
+++ components/detail/ListContent.tsx
@@ -37,24 +37,24 @@ const ListContent = ({ posts, pageName }: Props) => {
         <span className='dark:text-zinc-400 text-zinc-700'>{`Total (${posts?.length})`}</span>
       </div>
       {posts?.slice(index * offset, offset + index * offset).map((file, i) => (
-        <div key={i} className='mb-8'>
-          <div className='flex'>
-            <span>·</span>
-            <div className='pl-4'>
-              <Link href={`/posts/${pageName}/${file.slug}`}>
-                <span className='cursor-pointer inline-block hover:text-teal-600 hover:dark:text-teal-400 font-bold transition-all p-1 pl-0'>
+        <div key={i} className='mb-12 group'>
+          <div className='flex items-center'>
+            <div className='w-4 h-4 rounded-full bg-teal-800 group-hover:bg-teal-600 transition-all  shadow-md shadow-black' />
+            <Link href={`/posts/${pageName}/${file.slug}`}>
+              <div className='pl-4 cursor-pointer'>
+                <span className=' inline-block group-hover:text-teal-600 group-hover:dark:text-teal-400 font-bold transition-all p-1 pl-0'>
                   {file.title}
                 </span>
-              </Link>
-              <div className='flex flex-col space-y-1 text-xs dark:text-gray-400 text-gray-600'>
-                {file.createdAt && (
-                  <span>Created : {formateDate(file.createdAt)}</span>
-                )}
-                {file.updatedAt && (
-                  <span>Updated : {formateDate(file.updatedAt)}</span>
-                )}
+                <div className='flex flex-col space-y-1 text-xs dark:text-gray-400 text-gray-600'>
+                  {file.createdAt && (
+                    <span>Created : {formateDate(file.createdAt)}</span>
+                  )}
+                  {file.updatedAt && (
+                    <span>Updated : {formateDate(file.updatedAt)}</span>
+                  )}
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
         </div>
       ))}
